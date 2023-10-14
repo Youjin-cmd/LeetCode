554f663a9f480c3d274f4c92e5cc8cf16facae47
Youjin Kim <83858724+Youjin-cmd@users.noreply.github.com>
2023-10-14 15:43:01 +0900

    Time: 43 ms (93.24%), Space: 41.6 MB (89.12%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,14 +4,20 @@
  * @return {number}
  */
 const searchInsert = function(nums, target) {
-    let index = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] >= target) {
-            return i;
+    let low = 0;
+    let high = nums.length - 1;
+
+    while(low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if(nums[mid] == target) {
+            return mid;
+        } else if(nums[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
-        
-        index++;
     }
 
-    return index;
+    return high + 1;
 };
