b68d947dfc4afebcb9089f30e4a650b11723e041
Youjin Kim <83858724+Youjin-cmd@users.noreply.github.com>
2023-10-15 23:59:28 +0900

    Time: 66 ms (29.24%), Space: 44.5 MB (99.59%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+
+const dfs = function(curr, currentSum, targetSum) {
+    if (!curr) {
+        return false;
+    }
+
+    currentSum += curr.val;
+    
+    if (curr.left === null && curr.right === null) {
+        return currentSum === targetSum;
+    }
+    
+    return dfs(curr.left, currentSum, targetSum) || dfs(curr.right, currentSum, targetSum);
+}
+
+const hasPathSum = function(root, targetSum) {
+    return dfs(root, 0, targetSum);
+};
