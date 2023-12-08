ccd59159a7b34189b89b93b772362c9c1601fb9e
Youjin Kim <83858724+Youjin-cmd@users.noreply.github.com>
2023-12-08 16:24:48 +0900

    Time: 69 ms (71.24%), Space: 51.2 MB (91.15%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,33 +2,50 @@
  * @param {number[]} prices
  * @return {number}
  */
-const maxProfit = function(prices) {
-  let maxNum = 0;
-  let max = 0;
 
-  for (let i = 0; i < prices.length; i++) {
-    if (prices[i] >= prices[i + 1]) {
-      continue;
+// the clever answer
+
+function maxProfit(prices) {
+    let result = 0;
+    let min = prices[0];
+
+    for(let i = 1; i < prices.length; i++) {
+        min = Math.min(prices[i], min);
+        result = Math.max(result, prices[i] - min);
     }
-    
-    if (maxNum && maxNum - prices[i] < max) {
-      continue;
-    }
 
-    for (let j = i + 1; j < prices.length; j++) {
-      if (prices[j] > maxNum) {
-          maxNum = prices[j];
-      }
-
-      if (prices[i] > prices[j]) {
-          continue;
-      }
-      
-      if (prices[j] - prices[i] > max) {
-          max = prices[j] - prices[i];
-      }
-    }
-  }
-
-  return max;
+    return result;
 };
+
+// brute force..
+
+// function maxProfit(prices) {
+//   let maxNum = 0;
+//   let max = 0;
+
+//   for (let i = 0; i < prices.length; i++) {
+//     if (prices[i] >= prices[i + 1]) {
+//       continue;
+//     }
+    
+//     if (maxNum && maxNum - prices[i] < max) {
+//       continue;
+//     }
+
+//     for (let j = i + 1; j < prices.length; j++) {
+//       if (prices[j] > maxNum) {
+//           maxNum = prices[j];
+//       }
+
+//       if (prices[i] > prices[j]) {
+//           continue;
+//       }
+      
+//       if (prices[j] - prices[i] > max) {
+//           max = prices[j] - prices[i];
+//       }
+//     }
+//   }
+
+//   return max;
+// };
