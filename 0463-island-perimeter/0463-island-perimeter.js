03bf67786b25f643bab7e3b12a3c5a97ff51ea10
Youjin Kim <83858724+Youjin-cmd@users.noreply.github.com>
2023-12-27 23:56:30 +0900

    Time: 220 ms (9.36%), Space: 52.9 MB (16.18%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+
+var islandPerimeter = function(grid) {
+  let result = 0;
+  const visitedCells = new Set();
+  
+  function recursive(width, height) {
+    if (width < 0 || width >= grid.length || height < 0 || height >= grid[width].length || grid[width][height] === 0) {
+      result++;
+      return;
+    }
+
+    if (visitedCells.has(`${width}-${height}`)) {
+      return;
+    }
+
+    visitedCells.add(`${width}-${height}`);
+
+    recursive(width - 1, height);
+    recursive(width + 1, height);
+    recursive(width, height - 1);
+    recursive(width, height + 1);
+  }
+
+  for (let width = 0; width < grid.length; width++) {
+    for (let height = 0; height < grid[width].length; height++) {
+      if (grid[width][height]) {
+          recursive(width, height);
+      }
+    }
+  }
+  
+  return result;
+};
