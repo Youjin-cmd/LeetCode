5455fe272ee378f9d0422b2112a913360c365d89
Youjin Kim <83858724+Youjin-cmd@users.noreply.github.com>
2024-01-11 23:52:24 +0900

    Time: 64 ms (37.40%), Space: 45.3 MB (49.56%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) {
+        return 0;
+    }
+  
+    function dfs(current) {
+      if (!current) {
+            return 0;
+        }
+
+      return 1 + Math.max(dfs(current.left), dfs(current.right));
+    }
+  
+    return dfs(root);
+};
